a6bc6257a9d2f689d289478865ff41b302f1e469
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2023-09-03 11:29:28 +0200

    Created project structure and added BaseEntity

---
+++ Beemo-Server/Beemo-Server.Data/Models/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Beemo_Server.Data.Models.Entities
+{
+    [DataContract]
+    public class BaseEntity
+    {
+        [DataMember, Key] public int Id { get; set; }
+        [DataMember] public DateTime CreationDate { get; set; }
+        [DataMember] public DateTime? ModifiedDate { get; set; }
+        [DataMember] public bool Deprecated { get; set; }
+    }
+}
