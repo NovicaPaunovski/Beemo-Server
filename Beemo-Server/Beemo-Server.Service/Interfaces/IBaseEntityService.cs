142543d5c13a3bf79d130e6cc24974d3a005fb78
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2023-09-03 11:42:49 +0200

    Added BaseEntityService for handling entities

---
+++ Beemo-Server/Beemo-Server.Service/Interfaces/IBaseEntityService.cs
@@ -0,0 +1,11 @@
+﻿namespace Beemo_Server.Service
+{
+    public interface IBaseEntityService<TEntity, TRepository>
+    {
+        TEntity Create(TEntity entity);
+        TEntity Delete(TEntity entity);
+        TEntity Update(TEntity entity);
+        TEntity GetById(int id);
+        ICollection<TEntity> GetAll();
+    }
+}
