34c1cb5b89894774fc5198b0f9ba568b6284d218
Novica Paunovski <Novica.Paunovski@infineon.com>
2024-01-03 10:28:07 +0100

    Enabled CORS

--- Beemo-Server/Beemo-Server/Program.cs
+++ Beemo-Server/Beemo-Server/Program.cs
@@ -7,9 +7,21 @@ using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var allowBeemoOrigins = "_allowBeemoOrigins";
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// Enable CORS for Beemo Client
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(allowBeemoOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost", "http://localhost:3000", "http://localhost:3001")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -77,6 +89,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(allowBeemoOrigins);
 
 app.MapControllers();
 
